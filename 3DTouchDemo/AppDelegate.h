b5394cd5a9e71f32fabffb437480eb967128435a
pangkelu@duia.com <pangkelu@duia.com>
2016-03-10 09:53:25 +0800

    Initial Commit

---
+++ 3DTouchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3DTouchDemo
+//
+//  Created by 庞珂路 on 16/3/10.
+//  Copyright © 2016年 庞珂路. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
